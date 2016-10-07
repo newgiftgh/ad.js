43bc475dfad197b1dd41aeae30a49941e7f0d6e3
newgiftgh <megift@sina.com>
2016-10-08 00:37:55 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -369,7 +369,7 @@ function FindProxyForURL(url, host){
     ]
 
     dnsResolve("114.114.114.114");
-    var IS_AD = "PROXY 0.0.0.0";
+    var IS_AD = "PROXY 127.0.0.1";
     switch (mode){
       case 0:
           IS_NOT_AD = "DIRECT";
