8eb3d263ee4892f3f07b5d9311216b1877a82732
newgiftgh <megift@sina.com>
2016-10-23 21:41:49 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -4533,7 +4533,7 @@ function FindProxyForURL(url, host){
 //【在分界线上面可以追加URL规则，两边加上双引号，使用逗号分隔。】
     ]
 
-    dnsResolve("example.com");
+    dnsResolve("sc.cdce.cf");
     var IS_AD = "PROXY example.com:443";
     switch (mode){
       case 0:
